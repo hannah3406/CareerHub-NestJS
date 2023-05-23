715d01bbcc5387887ef8b00cd3a67d9051a8077f
hajungkim <hajung3406@naver.com>
2023-05-23 16:42:45 +0900

    삭제

--- build.sh
+++
@@ -1,3 +0,0 @@
-ssh -i ~/.ssh/ssh_key hajung3406@34.64.49.91 "cd careerhub/CareerHub-NestJS; sudo git checkout .; sudo git pull origin main; sudo rm -rf dist"
-scp -r /nestjs-backend/build hajung3406@34.64.49.91:
-ssh -i ~/.ssh/ssh_key hajung3406@34.64.49.91 "cd careerhub/CareerHub-NestJS; pm2 ecosystrm.config.js start"
